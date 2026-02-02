e7de80aace86faa42170ee258611412c2a309224
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-02 13:20:17 +0530

    commiting minor changes to the incomplete code

--- microheap.c
+++ microheap.c
@@ -32,16 +32,38 @@ header_t *head;
 header_t *tail;
 // these head and tail pointer is to keep track of the list
 
+void *get_free_block(size_t size) {
+    header_t *curr_ptr = head;
+    while(curr_ptr) {
+        if (curr_ptr->s.is_free && curr_ptr->s.size >= size) {
+            return curr_ptr;
+        }
+        curr_ptr = curr_ptr.s->next;
+    }
+    return NULL;
+}
+
 
 void *malloc(size_t size)  {
     if (!size)
         return NULL;
 
-	size_t total_size;
+	size_t total_size; // total size = size of the block + size of the header
+    total_size = size + sizeof(header_t);
+
 	void *block = NULL;
 	header_t *header;
 
 	pthread_mutex_lock(&global_malloc_lock);
+
+
+    header = get_free_block(total_size);
+
+    if (!header) {
+
+    }
+
+
 
 	block = sbrk(size);   //returns old program break (before change)
 	if (block == (void *)-1) {
