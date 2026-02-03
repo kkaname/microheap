25224e4e54f5e0e23f8d0c0324a382fdf62cf13a
Ayush Hegde <ayushhegde2005@gmail.com>
2026-02-04 04:29:02 +0530

    updated the malloc function as my_malloc to prevent clashing with library function
    Signed-off-by: Ayush Hegde <ayushhegde2005@gmail.com>

--- microheap.c
+++ microheap.c
@@ -1,78 +1,82 @@
-#include <stdio.h>
+#include "microheap.h"
+
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
 
-pthread_mutex_t global_malloc_lock;
+pthread_mutex_t global_malloc_lock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef char align[16];
+#define ALIGN16(x) (((x) + 15) & ~15)
 
-// this header for each allocated block
-// stores the size of a block and info that whether the block of memory is free or not
-typedef union memory_block_header {
-	struct {
-		size_t size;
-		unsigned is_free;
-		struct header_t *next;	// to make sure that each b
-					// allocated block is contiguous
-	} s;
-	align stub;
-} header_t;
 /*
  * this is the actual allocated block, where
- * the header is hidden from the calling function
- * ,--------------------,
- * |	  |Actual memory|
- * |header|    block	|
- * |      |             |
- * '--------------------'
+ * the header is hidder from the calling function
+ * ,------------------------.
+ * |        | Actual memory |
+ * | header |     block     |
+ * |        |               |
+ * '------------------------'
  */
+typedef union memory_block_header {
+    struct {
+        size_t size;
+        unsigned is_free;
+        union memory_block_header *next;
+    } s;
+    align stub;
+} header_t;
 
-header_t *head;
-header_t *tail;
-// these head and tail pointer is to keep track of the list
 
-void *get_free_block(size_t size) {
+header_t *head = NULL;
+header_t *tail = NULL;
+
+header_t *get_free_block(size_t size) {
     header_t *curr_ptr = head;
     while(curr_ptr) {
         if (curr_ptr->s.is_free && curr_ptr->s.size >= size) {
             return curr_ptr;
         }
-        curr_ptr = curr_ptr.s->next;
+        curr_ptr = curr_ptr->s.next;
     }
     return NULL;
 }
 
-
-void *malloc(size_t size)  {
-    if (!size)
+void *my_malloc(size_t size) {
+    if (!size) {
         return NULL;
-
-	size_t total_size; // total size = size of the block + size of the header
-    total_size = size + sizeof(header_t);
-
-	void *block = NULL;
-	header_t *header;
-
-	pthread_mutex_lock(&global_malloc_lock);
-
-
-    header = get_free_block(total_size);
-
-    if (!header) {
-
     }
 
+    size_t total_size;
+    total_size = ALIGN16(size + sizeof(header_t));
 
+    void *block = NULL;
+    header_t *header = NULL;
 
-	block = sbrk(size);   //returns old program break (before change)
-	if (block == (void *)-1) {
-		return NULL;
-    	}
-    	return block;
-}
+    pthread_mutex_lock(&global_malloc_lock);
 
-int main(void) {
+    header = get_free_block(size);
+    if (header) {
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void *)(header + 1);
+    }
 
-    return 0;
+    header = sbrk(total_size);
+    if (header == (void *) -1) {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+    if(!head) {
+        head = header;
+    }
+    if (tail) {
+        tail->s.next = header;
+    }
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+    return (void *)(header + 1);
 }
